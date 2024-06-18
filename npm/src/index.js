568678fb14a4e58a997f37994cd823d16b7e5a50
vitaliytv <v@nitra.ai>
2024-06-18 12:15:39 +0300

    eslint

--- npm/src/index.js
+++ npm/src/index.js
@@ -1,9 +1,7 @@
-import { env, checkEnv } from '@nitra/check-env'
-import { GraphQLClient, gql } from 'graphql-request'
+import { checkEnv, env } from '@nitra/check-env'
+import { GraphQLClient } from 'graphql-request'
 
 checkEnv(['QL', 'X_HASURA_ADMIN_SECRET'])
-
-export { gql, GraphQLClient }
 
 export const graphQLClient = new GraphQLClient(env.QL, {
   fetch,
@@ -11,3 +9,4 @@ export const graphQLClient = new GraphQLClient(env.QL, {
     'X-Hasura-Admin-Secret': env.X_HASURA_ADMIN_SECRET
   }
 })
+export { GraphQLClient, gql } from 'graphql-request'
